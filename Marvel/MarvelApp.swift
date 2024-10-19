84aca56d3eb85ca3705f36b1059b54e7f820b319
Luan Orlando <luanorlando@MacBook-Air-de-Luan.local>
2024-10-18 22:46:51 -0300

    feat: first commit with the project

---
+++ Marvel/MarvelApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelApp.swift
+//  Marvel
+//
+//  Created by Luan Orlando on 18/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MarvelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MarvelTests/MarvelTests.swift
@@ -0,0 +1,16 @@
+//
+//  MarvelTests.swift
+//  MarvelTests
+//
+//  Created by Luan Orlando on 18/10/24.
+//
+
+import Testing
+
+struct MarvelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
