ca22e1a06070f6e642cf45848d1d1d658eb3620f
luanorlando <orlandoluan@hotmail.com>
2024-10-19 00:48:39 -0300

    chore: adds test with button, will be removed

--- Marvel/ContentView.swift
+++ Marvel/ContentView.swift
@@ -8,12 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State
+    var buttonTitle: String = "Clique aqui!"
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Button(action: {
+                buttonTitle = "Button Clicked"
+            }) {
+                Text(buttonTitle)
+            }
+            .padding()
+            .border(.accent)
+            .cornerRadius(12)
+            .overlay {
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(.accent, lineWidth: 2.0)
+            }
         }
         .padding()
     }
