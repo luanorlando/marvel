cedece50b6ae3aa94df716d96f0243ef79285e39
luanorlando <orlandoluan@hotmail.com>
2024-11-26 13:45:38 -0300

    feat: adds CharactersViewModel

---
+++ Marvel/CharactersList/ViewModel/CharactersListViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  CharactersListViewModel.swift
+//  Marvel
+//
+//  Created by Luan Orlando on 26/11/24.
+//
+
+import MarvelRepository
+
+class CharactersListViewModel: CharactersListViewModelProtocol {
+    func fetchCharacters() {
+        let repository = MarvelRepository()
+        Task {
+            let characters = try await repository.fetchNewCharacters()
+            print(characters)
+        }
+    }
+}
---
+++ Marvel/CharactersList/ViewModel/CharactersListViewModelProtocol.swift
@@ -0,0 +1,10 @@
+//
+//  CharactersListViewModelProtocol.swift
+//  Marvel
+//
+//  Created by Luan Orlando on 26/11/24.
+//
+
+protocol CharactersListViewModelProtocol {
+    func fetchCharacters()
+}
